ae20e970187ec4affd5ffd1ff0a52e631a3b6337
Pasha <pashdoctor@gmail.com>
2021-09-05 15:29:23 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function superbowlWin(record){
+    const found = record.find(win => win.result === "W")
+    if(found != undefined){return found.year}
+else{return undefined}
+    
+}
 
+// if(found != undefined){return found.year}
+// else{returned undefined}
